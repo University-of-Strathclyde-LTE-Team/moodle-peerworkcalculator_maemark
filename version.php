aad33eb286557acda41a45e3bd5ae9ad97d9cd9f
ctriant1 <34750065+ctriant1@users.noreply.github.com>
2025-03-17 12:35:25 +0000

    Version update

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020052200;
+$plugin->version = 2025031700;
 $plugin->requires = 2019111200;  // Requires this Moodle version.
 $plugin->component = 'peerworkcalculator_maemark_cf';
